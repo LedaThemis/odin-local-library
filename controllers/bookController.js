89e5b3d1deeb3b7a87510b5e6cbc8fd4dcbe8b3e
Leda <ledathemis@protonmail.com>
2022-07-02 00:12:40 +0000

    implement book delete post

--- controllers/bookController.js
+++ controllers/bookController.js
@@ -201,8 +201,38 @@ exports.book_delete_get = function (req, res, next) {
 };
 
 // Handle book delete on POST.
-exports.book_delete_post = function (req, res) {
-  res.send('NOT IMPLEMENTED: Book delete POST');
+exports.book_delete_post = function (req, res, next) {
+  async.parallel(
+    {
+      book: (callback) => {
+        Book.findById(req.params.id).populate('author').exec(callback);
+      },
+      book_instances: (callback) => {
+        BookInstance.find({ book: req.params.id }).exec(callback);
+      },
+    },
+    (err, results) => {
+      if (err) return next(err);
+
+      // Book does not exist
+      if (results.book_instances.length > 0) {
+        res.render('book_delete', {
+          title: 'Delete Book',
+          book: results.book,
+          book_instances: results.book_instances,
+        });
+
+        return;
+      } else {
+        Book.findByIdAndRemove(req.body.bookid, (err) => {
+          if (err) return next(err);
+
+          // Success - go to book list.
+          res.redirect('/catalog/books');
+        });
+      }
+    }
+  );
 };
 
 // Display book update form on GET.

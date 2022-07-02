f3a441b1662b1a669395ddc7a16c21a19a19feeb
Leda <ledathemis@protonmail.com>
2022-07-02 00:38:54 +0000

    use env variables for mongodb URI

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(helmet());
 //
 //Set up mongoose connection
 var mongoose = require('mongoose');
-var mongoDB = 'mongodb+srv://ledathemis:hHIhAWdPFXhKouC8@sandbox.h7isvpk.mongodb.net/local_library?retryWrites=true&w=majority';
+var mongoDB = process.env.MONGODB_URI;
 mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));

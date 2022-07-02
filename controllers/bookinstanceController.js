561b550a72dd99c6aff6c7ccb65fa7f20ee371a4
Leda <ledathemis@protonmail.com>
2022-07-02 00:22:03 +0000

    implement book instance delete post

--- controllers/bookinstanceController.js
+++ controllers/bookinstanceController.js
@@ -98,19 +98,26 @@ exports.bookinstance_create_post = [
 
 // Display BookInstance delete form on GET.
 exports.bookinstance_delete_get = function (req, res, next) {
-  BookInstance.findById(req.params.id).populate('book').exec((err, bookinstance) => {
-    if (err) return next(err);
+  BookInstance.findById(req.params.id)
+    .populate('book')
+    .exec((err, bookinstance) => {
+      if (err) return next(err);
 
-    res.render('bookinstance_delete', {
-      title: 'Delete Book Instance',
-      bookinstance: bookinstance,
+      res.render('bookinstance_delete', {
+        title: 'Delete Book Instance',
+        bookinstance: bookinstance,
+      });
     });
-  });
 };
 
 // Handle BookInstance delete on POST.
 exports.bookinstance_delete_post = function (req, res) {
-  res.send('NOT IMPLEMENTED: BookInstance delete POST');
+  BookInstance.findByIdAndRemove(req.body.bookinstanceid, (err) => {
+    if (err) return next(err);
+
+    // Success - go to book instance list.
+    res.redirect('/catalog/bookinstances');
+  });
 };
 
 // Display BookInstance update form on GET.
